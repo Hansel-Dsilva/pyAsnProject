eeedf7136f2ef2245ee2c9e462ccd303daf5f462
Hansel D'silva <hanseldsilvag@gmail.com>
2019-07-12 20:22:28 +0530

    Update SDLasn1.py
    Improved main script

--- dist/SDLasn1.py
+++ dist/SDLasn1.py
@@ -1,13 +1,20 @@
+print("Press 0 to display question list")
 
-f = open('q.txt', 'r')
-print("Press Q to display question list")
+ls1 = []
+for i in range(66):
+    ls1.append(str(i))
 
 while 1:
     Qno = input("Enter question no. : ")
-    if Qno == 'Q' or Qno == 'q':
-        print(f.read())
-    elif int(Qno) in range(1, 66):
-        exec(open(Qno + ".py").read())
-    else:
-        print("INVALID INPUT! PLEASE REFER THE QUESTION LIST BY ENTERING 'Q'")
+    if Qno not in ls1:
+        print("INVALID INPUT! PLEASE REFER THE QUESTION LIST BY ENTERING '0'")
         continue
+    elif not int(Qno):
+        f = open('q.txt', 'r')
+        print(f.read())
+        f.close()
+    else:
+        try:
+            exec(open(Qno + ".py").read())
+        except FileNotFoundError:
+            print("Oops! Looks like " + Qno + " hasn't been solved yet.\nContribute by uploading " + Qno + ".py to https://github.com/Hansel-Dsilva/pyAsnProject/dist")
