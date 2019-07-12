8798664172aa1a4e2d73a4142dd0486f54e38def
Hansel D'silva <hanseldsilvag@gmail.com>
2019-07-12 18:42:18 +0530

    Create 47.py
    Added 47.py

---
+++ dist/47.py
@@ -0,0 +1,8 @@
+for i in range(5, 0 , -1):
+    for j in range(5, i-1, -1):
+        print("*", end = ' ')
+    print()
+for i in range(4):
+    for j in range(3, i-1, -1):
+        print("*", end = ' ')
+    print()
